f1e9a80ba6eaf5b8d167c2e0c641dcaaa3469779
Jonathan Cady <joncady@uw.edu>
2019-01-27 03:06:00 -0800

    Added tone playing and time adjustment

--- src/SendAudio.js
+++ src/SendAudio.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Alert } from 'reactstrap';
 import axios from 'axios';
-import * as tone from 'tone';
-// const p5 = require('./p5/p5')
+import * as Tone from 'tone';
 const rec = require('./Recorderjs-master/lib/recorder');
 
 export default class SendAudio extends Component {
@@ -10,7 +9,10 @@ export default class SendAudio extends Component {
     constructor() {
         super();
         this.state = {
-            url: "http://localhost:8000/audio"
+            url: "http://localhost:8000/audio",
+            bar: '',
+            errorMessage: null,
+            player: null
         }
     }
 
@@ -18,8 +20,13 @@ export default class SendAudio extends Component {
         const { url } = this.state;
         var constraints = { audio: true, video: false }
         var audioContext = new AudioContext;
+        let player = new Tone.Player('http://localhost:8000/song?name=bach.mp3').toMaster();
+        this.setState({
+            player: player
+        });
+        player.autostart = true;
         navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-            console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+            console.log("getUserMedia() success, strem created, initializing Recorder.js ...");
 
             /* assign to gumStream for later use */
             // gumStream = stream;
@@ -46,9 +53,26 @@ export default class SendAudio extends Component {
         mic.exportWAV((file) => {
             let form = new FormData();
             form.append("data", file);
+            this.setState({
+                errorMessage: null
+            });
             axios.post(url, form, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
+                }
+            }).then((res) => {
+                let dataObject = res.data;
+                if (dataObject.type == "location") {
+                    this.setState({
+                        bar: dataObject.bar
+                    });
+                    this.state.player.seek(dataObject.bar);
+                } else if (dataObject.type == "stop") {
+                    console.log("Playback");
+                } else {
+                    this.setState({
+                        errorMessage: "No command heard."
+                    })
                 }
             })
         });
@@ -58,7 +82,11 @@ export default class SendAudio extends Component {
         return (
             <div>
                 <input id="send-audio" type="image" onClick={this.startStream} src={require('./assets/music.png')} />
-                <input type="file" accept="audio/*" capture></input>
+                <p>{this.state.bar}</p>
+                {this.state.errorMessage &&
+                <Alert color="danger">
+                    {this.state.errorMessage}
+                </Alert>}
             </div>
         );
     }
